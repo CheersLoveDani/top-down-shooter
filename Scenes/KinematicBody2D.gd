90a933adbcfdcff353f10efcc94152c512db556f
Ewan Borland <43387200+EwanBorland834@users.noreply.github.com>
2022-04-09 16:02:32 +0100

    wasd movement

---
+++ Scenes/KinematicBody2D.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var speed = 200  # speed in pixels/sec
+var velocity = Vector2.ZERO
+
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	# Make sure diagonal movement isn't faster
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
